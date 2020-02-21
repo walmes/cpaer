554c0ba10244bd898aa4138d1a644a34cb5df5e9
Walmes Zeviani <walmes@ufpr.br>
2020-02-21 09:30:01 -0300

    Updates ancova.

--- scripts/06-ancova.R
+++ scripts/06-ancova.R
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------
 # Curso em Planejamento e Análise de Experimentos com R
-#   · Análise Exploratória de Dados Experimentais
+#   · Análise de Covariância
 #
 #                                            Prof. Dr. Walmes M. Zeviani
 #                                leg.ufpr.br/~walmes · github.com/walmes
@@ -15,6 +15,7 @@
 
 library(tidyverse)
 library(labestData)
+library(emmeans)
 
 # Análise de covariância.
 BanzattoQd9.2.1
@@ -29,3 +30,49 @@ ZimmermannTb14.3
 
 #-----------------------------------------------------------------------
 #
+
+tb <- ZimmermannTb14.3
+str(tb)
+
+ggplot(data = tb,
+       mapping = aes(x = reorder(cult, stand),
+                     y = stand)) +
+    geom_point()
+
+ggplot(data = tb,
+       mapping = aes(x = cult,
+                     y = prod,
+                     color = bloc,
+                     size = stand)) +
+    geom_point()
+
+m0 <- lm(prod ~ bloc + stand + cult, data = tb)
+
+par(mfrow = c(2, 2))
+plot(m0)
+layout(1)
+
+anova(m0)
+summary(m0)
+
+drop1(m0, test = "F")
+
+mean(tb$stand)
+
+tb_means <- emmeans(m0, specs = ~cult, at = list(stand = 100)) %>%
+    multcomp::cld()
+
+ggplot(data = tb_means,
+       mapping = aes(x = reorder(cult, emmean), y = emmean)) +
+    geom_point() +
+    geom_errorbar(mapping = aes(ymin = lower.CL, ymax = upper.CL),
+                  width = 0) +
+    geom_text(mapping = aes(label = sprintf("%0.0f", emmean)),
+              hjust = 0,
+              nudge_x = 0.05)
+
+#-----------------------------------------------------------------------
+
+url <- "http://leg.ufpr.br/~walmes/data/castracao.txt"
+tb <- read_tsv(url, comment = "#")
+str(tb)
