0ada1cf0146bc58a55c9b870bfb504647c3a7670
Walmes Zeviani <walmes@ufpr.br>
2020-02-21 08:43:52 -0300

    Análise de covariância.

---
+++ scripts/06-ancova.R
@@ -0,0 +1,31 @@
+#-----------------------------------------------------------------------
+# Curso em Planejamento e Análise de Experimentos com R
+#   · Análise Exploratória de Dados Experimentais
+#
+#                                            Prof. Dr. Walmes M. Zeviani
+#                                leg.ufpr.br/~walmes · github.com/walmes
+#                                        walmes@ufpr.br · @walmeszeviani
+#                      Laboratory of Statistics and Geoinformation (LEG)
+#                Department of Statistics · Federal University of Paraná
+#                                       2020-fev-18 · Curitiba/PR/Brazil
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# Pacotes.
+
+library(tidyverse)
+library(labestData)
+
+# Análise de covariância.
+BanzattoQd9.2.1
+PimentelTb16.2.1
+PimentelTb16.3.1
+RamalhoEg13.2
+RamalhoEx13.1
+RamalhoEx13.2
+RamalhoEx13.3
+RamalhoTb13.6
+ZimmermannTb14.3
+
+#-----------------------------------------------------------------------
+#
